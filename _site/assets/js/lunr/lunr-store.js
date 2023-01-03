e86d2a3fbf68f4875686fa1534d4ba41d5bb112c
sangjaeoh <sangjaeoh91@gmail.com>
2023-01-04 00:44:09 +0900

    add posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,8 +12,20 @@ var store = [{
         "teaser": null
       },{
         "title": "우아한테크캠프 Pro 5기 소개 및 커리큘럼",
-        "excerpt":"10월 24일 ~ 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 우아한테크캠프 Pro 소개 NextStep 교육 과정으로 9주 동안 8개 미션을 진행을 합니다. 각 미션별 코드 리뷰어가 배정 되며 미션을 진행하고, 진행한 미션에 대해 코드 리뷰를 받는식으로 강의를 진행 합니다. 🎯 우아한테크캠프 Pro 과정의 학습 목표 클린코드를 통해 유지보수하기...","categories": ["etc","education"],
+        "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 우아한테크캠프 Pro 소개 NextStep 교육 과정으로 9주 동안 8개 미션을 진행을 합니다. 각 미션별 코드 리뷰어가 배정 되며 미션을 진행하고, 진행한 미션에 대해 코드 리뷰를 받는식으로 강의를 진행 합니다. 🎯 우아한테크캠프 Pro 과정의 학습 목표 클린코드를...","categories": ["etc","education"],
         "tags": ["education","우아한테크캠프 pro 5기"],
         "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-%EC%86%8C%EA%B0%9C/",
+        "teaser": null
+      },{
+        "title": "우아한테크캠프 Pro 5기 - 1주차 (로또 TDD)",
+        "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 로또 구입 금액을 입력하면, 구입 금액에 해당하는 로또를 발급하는 애플리케이션을 만드는 미션 🎯 학습 목표 TDD 기반으로 프로그래밍하는 경험 메소드 분리 + 클래스를 분리하는 리팩토링 경험 점진적으로 리팩토링하는 경험 객체지향 생활 체조 원칙 경험...","categories": ["etc","education"],
+        "tags": ["education","우아한테크캠프 pro 5기"],
+        "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-1%EC%A3%BC%EC%B0%A8/",
+        "teaser": null
+      },{
+        "title": "우아한테크캠프 Pro 5기 - 2주차 (JPA)",
+        "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 QnA 저장소를 기반으로 JPA를 사용하여 엔티티 매핑, 연관 관계 매핑, 서비스 레이어 리팩터링을 진행하는 미션 🎯 학습 목표 JPA 학습 테스트를 통한 JPA 학습 Q&amp;A 서비스를 JPA로 매핑 📖 강의 내용 JPA에 대해 전반적인...","categories": ["etc","education"],
+        "tags": ["education","우아한테크캠프 pro 5기"],
+        "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-2%EC%A3%BC%EC%B0%A8/",
         "teaser": null
       }]
