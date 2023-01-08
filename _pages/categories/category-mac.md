65236aba91152e0e4c6e2645175a1676190acc86
sangjaeoh <sangjaeoh91@gmail.com>
2023-01-08 17:48:37 +0900

    add posts

---
+++ _pages/categories/category-mac.md
@@ -0,0 +1,13 @@
+---
+title: "mac"
+layout: archive
+permalink: categories/mac
+author_profile: true
+sidebar_main: true
+sidebar:
+    nav: "docs"
+---
+
+
+{% assign posts = site.categories['mac'] %}
+{% for post in posts %} {% include archive-single.html type=page.entries_layout %} {% endfor %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -13,43 +13,55 @@ var store = [{
       },{
         "title": "우아한테크캠프 Pro 5기 소개 및 커리큘럼",
         "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 우아한테크캠프 Pro 소개 NextStep 교육 과정으로 9주 동안 8개 미션을 진행을 합니다. 각 미션별 코드 리뷰어가 배정 되며 미션을 진행하고, 진행한 미션에 대해 코드 리뷰를 받는식으로 강의를 진행 합니다. 🎯 우아한테크캠프 Pro 과정의 학습 목표 클린코드를...","categories": ["etc","education"],
-        "tags": ["education","우아한테크캠프 pro 5기"],
+        "tags": ["우아한테크캠프 pro 5기"],
         "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-%EC%86%8C%EA%B0%9C/",
         "teaser": null
       },{
         "title": "우아한테크캠프 Pro 5기 - 1주차 (로또 TDD)",
         "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 로또 구입 금액을 입력하면, 구입 금액에 해당하는 로또를 발급하는 애플리케이션을 만드는 미션 🎯 학습 목표 TDD 기반으로 프로그래밍하는 경험 메소드 분리 + 클래스를 분리하는 리팩토링 경험 점진적으로 리팩토링하는 경험 객체지향 생활 체조 원칙 경험...","categories": ["etc","education"],
-        "tags": ["education","우아한테크캠프 pro 5기"],
+        "tags": ["우아한테크캠프 pro 5기"],
         "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-1%EC%A3%BC%EC%B0%A8/",
         "teaser": null
       },{
         "title": "우아한테크캠프 Pro 5기 - 2주차 (JPA)",
         "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 QnA 저장소를 기반으로 JPA를 사용하여 엔티티 매핑, 연관 관계 매핑, 서비스 레이어 리팩터링을 진행하는 미션 🎯 학습 목표 JPA 학습 테스트를 통한 JPA 학습 Q&amp;A 서비스를 JPA로 매핑 📖 강의 내용 JPA에 대해 전반적인...","categories": ["etc","education"],
-        "tags": ["education","우아한테크캠프 pro 5기"],
+        "tags": ["우아한테크캠프 pro 5기"],
         "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-2%EC%A3%BC%EC%B0%A8/",
         "teaser": null
       },{
         "title": "우아한테크캠프 Pro 5기 - 3주차 (인수 테스트 주도 개발 - ATDD)",
         "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 인수 테스트 만들기 경험을 위해 지하철 노선도 관리 어드민 서비스 구현을 제공된 인수 조건 기반으로, 인수 테스트 주도 개발을 진행하는 미션 🎯 학습 목표 웹 애플리케이션을 인수 테스트 주도 개발 방법으로 개발하는 경험 📖...","categories": ["etc","education"],
-        "tags": ["education","우아한테크캠프 pro 5기"],
+        "tags": ["우아한테크캠프 pro 5기"],
         "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-3%EC%A3%BC%EC%B0%A8/",
         "teaser": null
       },{
         "title": "우아한테크캠프 Pro 5기 - 4주차 (그럴듯한 서비스 만들기)",
         "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 AWS에서 네트워크를 구성해보고 서비스를 배포하며 네트워크에 대한 기본적인 개념을 학습해보는 미션 🎯 학습 목표 AWS 상에서 네트워크를 구성하며, 네트워크 기본 개념들을 학습 도커 컨테이너를 학습 개발 환경을 구성해보고 지속적 통합을 경험 📖 강의 내용...","categories": ["etc","education"],
-        "tags": ["education","우아한테크캠프 pro 5기"],
+        "tags": ["우아한테크캠프 pro 5기"],
         "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-4%EC%A3%BC%EC%B0%A8/",
         "teaser": null
       },{
         "title": "우아한테크캠프 Pro 5기 - 5주차 (인수 테스트 기반 TDD)",
         "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 지하철 노선도 서비스을 구현하면서 인수 테스트 기반 TDD를 진행하는 미션 🎯 학습 목표 인수 테스트 기반의 TDD 경험 로그인 상태 인수 테스트 📖 강의 내용 인수 테스트 기반으로 TDD를 진행하는 방법을 설명하고, 단위테스트와 통합테스트...","categories": ["etc","education"],
-        "tags": ["education","우아한테크캠프 pro 5기"],
+        "tags": ["우아한테크캠프 pro 5기"],
         "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-5%EC%A3%BC%EC%B0%A8/",
         "teaser": null
       },{
         "title": "우아한테크캠프 Pro 5기 - 6주차 (서비스 진단하기)",
         "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 웹 성능 테스트, 부하 테스트 및 성능 모니터링을 직접 해보는 미션 🎯 학습 목표 웹 성능테스트 부하테스트 모니터링 📖 강의 내용 웹 성능테스트, 부하 테스트 및 모니터링의 설명과 함께 PageSpeed, k6, Cloudwatch 등을 이용해...","categories": ["etc","education"],
-        "tags": ["education","우아한테크캠프 pro 5기"],
+        "tags": ["우아한테크캠프 pro 5기"],
         "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-6%EC%A3%BC%EC%B0%A8/",
+        "teaser": null
+      },{
+        "title": "우아한테크캠프 Pro 5기 - 7주차 (레거시 코드 리팩터링)",
+        "excerpt":"2022년 10월 24일 ~ 2022년 12월 23일 우아한테크캠프 Pro 5기를 경험한 내용 기록입니다. 🙇🏻‍♂️ 미션 설명 레거시 프로젝트를 리팩터링하는 미션 🎯 학습 목표 레거시 프로젝트를 리팩터링하는 경험을 통해 서비스를 안정적으로 운영하면서 레거시 코드를 리팩터링할 수 있는 역량을 키운다. 프로젝트를 만드는 단계에서 끝나는 것이 아니라 프로젝트를 완료한 후 일정 기간 유지보수를...","categories": ["etc","education"],
+        "tags": ["우아한테크캠프 pro 5기"],
+        "url": "/etc/education/%EC%9A%B0%EC%95%84%ED%95%9C%ED%85%8C%ED%81%AC%EC%BA%A0%ED%94%84pro5%EA%B8%B0-7%EC%A3%BC%EC%B0%A8/",
+        "teaser": null
+      },{
+        "title": "MAC 단축키 모음 💻",
+        "excerpt":"   화면 잠금: Control + Command + Q  ","categories": ["etc","mac"],
+        "tags": ["mac","단축키"],
+        "url": "/etc/mac/hotkey/",
         "teaser": null
       }]
